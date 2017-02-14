1c2e3f975056001841ff025f92cfbfa8bb2490ca
ChristopherM <stanordan@gmail.com>
2017-02-14 11:03:37 -0500

    added read more link and changed version number

--- functions.php
+++ functions.php
@@ -2,3 +2,8 @@
     return preg_replace('/<p([^>]+)?>/', '<p$1 class="lead">', $content, 1);
 }
 add_filter('the_content', 'first_paragraph'); ?>
+
+<?php function new_excerpt_more( $more ) {
+	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More&hellip;', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' ); ?>
